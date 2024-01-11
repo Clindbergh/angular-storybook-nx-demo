ad843c1d5a6eec36379195d9562615e52403a220
Patrick Hodel <patrick@intemp.ch>
2024-01-11 13:42:11 +0100

    Add a test component

---
+++ src/app/test-component1/test-component1.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'storybook-demo-test-component1',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './test-component1.component.html',
+  styleUrl: './test-component1.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TestComponent1Component {}
